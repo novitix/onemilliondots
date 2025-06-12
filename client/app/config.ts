b876730a1942a3ae5d6afcd2b418027d894e1842
Adam Ye <a2d0a0m2@gmail.com>
2025-06-12 19:42:23 +1000

    use config json

--- client/app/config.ts
+++ client/app/config.ts
@@ -1,5 +1,7 @@
+const configJson = await import("../config/config.json");
+
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL,
+  apiUrl: configJson.apiUrl,
   refetchInterval: 1000, // ms
   canvasWidth: 1000,
   canvasHeight: 1000,
